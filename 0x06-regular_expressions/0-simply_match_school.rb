a822c5cbd420248e33ca32b16a224a6957dee6c2
InemesitMatthew <inemesit.e.matthew@gmail.com>
2024-01-06 01:45:48 +0100

    regexp

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,13 +1,3 @@
 #!/usr/bin/env ruby
 
-# Accept one argument from the command line
-input_string = ARGV[0]
-
-# Define the regular expression to match "School"
-regex_pattern = /School/
-
-# Use the scan method to find all occurrences of the pattern
-matches = input_string.scan(regex_pattern)
-
-# Print the matches (if any)
-puts matches.join
+puts ARGV[0].scan(/School/).join
