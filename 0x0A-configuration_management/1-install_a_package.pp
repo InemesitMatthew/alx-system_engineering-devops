d4f4728f93ea5eecb28ae920e5697229591e6be7
InemesitMatthew <inemesit.e.matthew@gmail.com>
2024-02-24 20:04:15 +0100

    flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,13 @@
 # 1-install_a_package.pp
 # Puppet manifest to install Flask from pip3
 
-# Install Flask using pip3
+# Install Flask and specify Werkzeug version
 package { 'Flask':
   ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
   provider => 'pip3',
 }
